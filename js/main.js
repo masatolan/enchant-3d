20ba8ca9bbca1073ca033c897e8745efa8d82abc
masatolan <kondo0310@gmail.com>
2015-06-27 16:24:39 +0900

    first commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+enchant();
+
+window.onload = function() {
+	var game = new Game(640, 640);
+	game.preload('img/enchant.png');
+
+	game.onload = function() {
+		var scene = new Scene3D();
+		var cube = new Cube();
+		cube.mesh.texture.src = game.assets['img/enchant.png'];
+
+		cube.onenterframe = function() {
+			cube.rotateRoll(0.1);
+			cube.rotateYaw(0.1);
+		}
+		scene.addChild(cube);
+	}
+
+	game.start();
+}
